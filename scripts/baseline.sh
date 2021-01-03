0afed2ae5d174b8d889af381fa6d9c61c073a264
Rajdeep <rajdeep.sarkar@insight-centre.org>
2021-01-03 01:07:57 +0000

    folders cached

--- scripts/baseline.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-let num_runs=$1
-let gpu_id=$2
-
-for i in $(seq 0 $((num_runs-1)));
-do
-    CUDA_VISIBLE_DEVICES=$gpu_id python parlai/tasks/redial/train_autorec.py -mf saved/release_baseline_$i
-done
--- scripts/t2t_rec_rgcn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-let num_runs=$1
-let gpu_id=$2
-
-for i in $(seq 0 $((num_runs-1)));
-do
-    CUDA_VISIBLE_DEVICES=$gpu_id python parlai/tasks/redial/train_transformer_rec.py -mf saved/transformer_rec_both_rgcn_$i
-done
-
